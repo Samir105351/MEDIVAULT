b438a727de980be91bdea8b4fcbce735e7e5ce21
Samir105351 <74838959+Samir105351@users.noreply.github.com>
2024-06-28 02:06:41 +0600

    Introduce JWTAuthenticationFilter and JwtService

---
+++ src/main/java/com/samir/medivault/config/JWTAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package com.samir.medivault.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if(Objects.isNull(authHeader) || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+    }
+}
---
+++ src/main/java/com/samir/medivault/config/JwtService.java
@@ -0,0 +1,12 @@
+package com.samir.medivault.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+    public String extractUsername(String token){
+        return null;
+
+    }}
