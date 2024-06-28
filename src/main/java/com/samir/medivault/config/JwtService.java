d14493fbea4213f4996ed89d766e09455f299764
Samir105351 <74838959+Samir105351@users.noreply.github.com>
2024-06-28 22:09:23 +0600

    wip

--- src/main/java/com/samir/medivault/config/JwtService.java
+++ src/main/java/com/samir/medivault/config/JwtService.java
@@ -1,12 +1,38 @@
 package com.samir.medivault.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
 
 @Service
 @RequiredArgsConstructor
 public class JwtService {
+
+    private static final String SECRET_KEY = "c80c28198faa1322bc79744d0ae7d666a1e37c59809fd113844c7c9a24628eeb";
+
     public String extractUsername(String token){
         return null;
 
-    }}
+    }
+
+    private Claims ExtractClaims(String token){
+        return Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
+
+
