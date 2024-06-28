f0b946410eadcedbccf3a907e20bac7f0b4cc09e
Samir105351 <74838959+Samir105351@users.noreply.github.com>
2024-06-29 02:00:36 +0600

    Remove PatientInformation as it is part of PrescriptionDetails

--- src/main/java/com/samir/medivault/dto/prescription/PrescriptionResponse.java
+++ src/main/java/com/samir/medivault/dto/prescription/PrescriptionResponse.java
@@ -1,4 +1,18 @@
 package com.samir.medivault.dto.prescription;
 
-public class PrescriptionResponse {
+import com.samir.medivault.enums.Gender;
+import com.samir.medivault.enums.Status;
+
+import java.util.Date;
+
+public record PrescriptionResponse(
+        Date PrescriptionDate,
+        String patientName,
+        Long patientAge,
+        Gender patientGender,
+        String diagnosis,
+        String medicine,
+        Date nextVisitDate,
+        Status status
+) {
 }
